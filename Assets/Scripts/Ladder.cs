1b62717c1d05caa7fc6567668ecad01788ddbd8d
zezzeebka <72992258+zezzeebka@users.noreply.github.com>
2020-12-16 02:29:43 +0300

    /

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public GameObject TrigerBox;
+    public GameObject Ladder1; 
+    void OnTriggerEnter2D(Collider2D other)
+    {   
+        if (other.tag == "Player")
+        {
+            Ladder1.GetComponent<EdgeCollider2D>().offset = new Vector2(0, 0);
+        }
+    }
+}
+    /*void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            detected = false;
+        }
+    }*/
+
