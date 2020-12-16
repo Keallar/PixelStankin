2d59b3eb0c925eebd8aab57dd5ba3882d64a661d
zezzeebka <72992258+zezzeebka@users.noreply.github.com>
2020-12-16 04:31:29 +0300

    .

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    public GameObject TrigerBox;
-    public GameObject Ladder1; 
-    void OnTriggerEnter2D(Collider2D other)
-    {   
+    public GameObject Ladder1;
+    void OnTriggerStay2D(Collider2D other)
+    {
         if (other.tag == "Player")
         {
-            Ladder1.GetComponent<EdgeCollider2D>().offset = new Vector2(0, 0);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Ladder1.GetComponent<EdgeCollider2D>().offset = new Vector2(0, 0);
+            }
         }
-    }
+        if (other.tag == "Player")
+        {
+        if (Input.GetKey(KeyCode.DownArrow))       
+            {
+                Ladder1.GetComponent<EdgeCollider2D>().offset = new Vector2(10, 0); 
+            }
+        }
+    }   
 }
     /*void OnTriggerExit(Collider other)
     {
